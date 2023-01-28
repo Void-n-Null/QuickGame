83e244ca3570362acc71a9006c8cec1c59cc7322
Void&Null <kosmix1999@gmail.com>
2023-01-27 21:18:02 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,9 +5,9 @@ public static class ModHelperData
     public const string Name = "Quick Game";
     public const string Description = "Loads into game immediately after Title Screen. No more clicking into sandbox mode over and over.";
     public const string Version = "1.0.0";
-    /*    public const string RepoName = "BTD6-TF2-Aimbot-Tower";*/
+    public const string RepoName = "QuickGame";
     public const string RepoOwner = "Void-n-Null";
-    /*    public const string DllName = "TF2AimbotSniper.dll";*/
+    public const string DllName = "QuickGame.dll";
     public const string Author = "Void & Null";
     public const string WorksOnVersion = "34.3";
 }
