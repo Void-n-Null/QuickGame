ce6624e82c69b50ca8094d8ff064ad5e8c20a6c6
Void&Null <kosmix1999@gmail.com>
2023-01-27 21:12:45 -0700

    Initial commit

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace Patzilla;
+
+public static class ModHelperData
+{
+    public const string Name = "Quick Game";
+    public const string Description = "Load into a game right after BTD6 starts! No more clicking into sandbox! \n (Modding Utility)";
+    public const string Version = "1.0.0";
+    /*    public const string RepoName = "BTD6-TF2-Aimbot-Tower";*/
+    public const string RepoOwner = "Void-n-Null";
+    /*    public const string DllName = "TF2AimbotSniper.dll";*/
+    public const string Author = "Void & Null";
+    public const string WorksOnVersion = "34.3";
+}
---
+++ QuickGame.cs
@@ -0,0 +1,50 @@
+using MelonLoader;
+using BTD_Mod_Helper;
+using QuickGame;
+using Il2CppAssets.Scripts.Unity.UI_New;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using UnityEngine;
+using UnityEngine.UI;
+using BTD_Mod_Helper.Api.ModOptions;
+using System;
+
+[assembly: MelonInfo(typeof(QuickGame.QuickGame), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace QuickGame;
+
+public class QuickGame : BloonsTD6Mod
+{
+    public bool GameStart = true;
+/*    public enum diff {Easy,Medium,Hard}
+    public enum mode {Sandbox,Standard,MagicOnly,Impoppable,Chimps,HalfCash}
+    public ModSettingEnum<diff> Difficulty = new ModSettingEnum<diff>(diff.Easy);
+    public ModSettingEnum<mode> Mode = new ModSettingEnum<mode>(mode.Sandbox);*/
+    public override void OnApplicationStart()
+    {
+
+    }
+
+    public override void OnMainMenu()
+    {
+        if (GameStart)
+        {
+/*            var mode = Difficulty.GetValue().ToString();
+            if (mode == "Chimps")
+            {
+                mode = "Clicks";
+            }*/
+            InGameData.Editable.selectedMode = "Sandbox";
+            InGameData.Editable.selectedMap = "Tutorial";
+            InGameData.Editable.selectedDifficulty = "Easy";
+            UI.instance.LoadGame();
+            GameStart = false;
+        }
+    }
+
+    public override void OnTitleScreen()
+    {
+        var button = GameObject.Find("Canvas/ScreenBoxer/TitleScreen/Start");
+        button.GetComponent<Button>().onClick.Invoke();
+    }
+}
